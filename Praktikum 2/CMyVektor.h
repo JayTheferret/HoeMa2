e8f3ae022753f031f52fa7dfe4f06c3ba4d480af
Jay <57915168+JayTheferret@users.noreply.github.com>
2021-05-10 16:14:32 +0200

    initial commit
     (copied over class from "Praktikum1"

---
+++ Praktikum 2/CMyVektor.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <vector>
+#include <math.h>
+
+//iomanip -> ausgabe anpassen (kommazahlen anpassen)
+
+class CMyVektor {
+private:
+	std::vector<double> vector; //vector mir stl
+	int dimension;
+
+public:
+
+	CMyVektor(int dimension); //konstruktor
+
+	int get_dimension();
+	void set_component(int component, double value);
+	double get_component(int component);
+
+	double vector_length(CMyVektor vector);
+	void print_vector(CMyVektor vector);
+
+	double& operator[](int index) { return vector[index]; };
+};
+
+CMyVektor operator+(CMyVektor a, CMyVektor b); //Vektor addition -> ueverschreibt + fuer vektoren
+CMyVektor operator*(double lambda, CMyVektor a); //Vektor multiplikation -> ueberschreibt * fuer Vektoren(CMyVector) -> Reihenfolge wichtig! lambda*a != a*lambda (funktion umdrehen)
+
+//Aufgabe 2
+
+double fvx(CMyVektor x);
+double gvx(CMyVektor x);
+
+CMyVektor gradient(CMyVektor x, double(*funktion)(CMyVektor x));
+
+//Aufgabe 3
+
+CMyVektor g_verfahren(CMyVektor x, double(*funktion)(CMyVektor x), double lambda);
+CMyVektor g_verfahren(CMyVektor x, double(*funktion)(CMyVektor x)); //default lambda = 1.0
---
+++ Praktikum 2/main.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include "CMyVektor.h"
+
+int main() {
+
+	//Praktikum eins, Vektorbeispielaufgaben
+
+	//------------------------
+	/*
+	CMyVektor x(3);
+	x.set_component(0, 0);
+	x.set_component(1, 0);
+	x.set_component(2, 0);
+
+	g_verfahren(x, gvx, 0.1);
+	*/
+	//------------------------
+
+	/*CMyVektor x(2);
+	x.set_component(0, 0.2);
+	x.set_component(1, -2.1);
+
+	g_verfahren(x, fvx);
+*/
+	return 0;
+	system("PAUSE");
+}
