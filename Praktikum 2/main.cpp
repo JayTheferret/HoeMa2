a95a65d6697c3ebd4daf0683e32171b00ffe2234
Jay <57915168+JayTheferret@users.noreply.github.com>
2021-05-11 18:25:41 +0200

    update main.cpp
    made easy to show test outputs

--- Praktikum 2/main.cpp
+++ Praktikum 2/main.cpp
@@ -3,72 +3,30 @@
 #include "CMyMatrix.h"
 
 int main() {
+	
+	//-------------------------------------------------------------------------------------------
+	//Aufgabe 1
 
-	//Praktikum eins, Vektorbeispielaufgaben
+	//-------------------------------------------------------------------------------------------
+	//inverse Matrix Beispiel
 
-	//------------------------
-	/*
-	CMyVektor x(3);
-	x.set_component(0, 0);
-	x.set_component(1, 0);
-	x.set_component(2, 0);
+	/*CMyMatrix m_1(2,2);
+	m_1.set_component(0, 0, 1);
+	m_1.set_component(0, 1, 2);
+	m_1.set_component(1, 0, 3);
+	m_1.set_component(1, 1, 4);
 
-	g_verfahren(x, gvx, 0.1);
-	*/
-	//------------------------
+	m_1.print_matrix(m_1);
 
-	/*CMyVektor x(2);
-	x.set_component(0, 0.2);
-	x.set_component(1, -2.1);
+	std::cout << "Dimension: " << m_1.get_dimension_m() << "X" << m_1.get_dimension_n() << std::endl;
 
-	g_verfahren(x, fvx);
-*/
+	std::cout << "inverse Matrix: " << std::endl;
+	m_1.print_matrix(m_1.invers(m_1));*/
 
-	//CMyMatrix m_1(2,2);
-	//m_1.set_component(0, 0, 1);
-	//m_1.set_component(0, 1, 2);
-	//m_1.set_component(1, 0, 3);
-	//m_1.set_component(1, 1, 4);
-
-	//m_1.print_matrix(m_1);
-
-	//std::cout << "Dimension: " << m_1.get_dimension_m() << "X" << m_1.get_dimension_n() << std::endl;
-
-	//std::cout << "inverse Matrix: " << std::endl;
-	//m_1.print_matrix(m_1.invers(m_1));
-	////m_1.invers(m_1);
-
-	/*std::cout << "Matrix Vektor Multiplikation: ";
-
-	CMyVektor v_1(2);
-	v_1.set_component(0, 1);
-	v_1.set_component(1, 2);
-
-	CMyVektor mult(2);
-	mult = m_1 * v_1;
-	mult.print_vector(mult);*/
-
-	////Beispiel Aufgabe 2
-	/*CMyVektor x(4);
-	x.set_component(0, 1);
-	x.set_component(1, 2);
-	x.set_component(2, 0);
-	x.set_component(3, 3);
-
-	CMyMatrix j_matrix = jacobi(x, f_1);
-	j_matrix.print_matrix(j_matrix);
-*/
-	//aufgabe 3
-	/*CMyektor x(2);
-	x.set_component(0, 1);
-	x.set_component(1, 1);
-
-	newton_verfahren(x, f_2);
-*/
-
+	//-------------------------------------------------------------------------------------------
 	//Mult test
 
-	CMyMatrix mult(2, 3);
+	/*CMyMatrix mult(2, 3);
 	mult.set_component(0, 0, 4);
 	mult.set_component(0, 1, 3);
 	mult.set_component(0, 2, 2);
@@ -88,8 +46,32 @@ int main() {
 	mult_v.print_vector(mult_v);
 
 	std::cout << "=" << std::endl;
-	mult_v.print_vector(mult*mult_v);
+	mult_v.print_vector(mult*mult_v);*/
 
+
+	//-------------------------------------------------------------------------------------------
+	//Aufgabe 2
+
+	/*CMyVektor x(4);
+	x.set_component(0, 1);
+	x.set_component(1, 2);
+	x.set_component(2, 0);
+	x.set_component(3, 3);
+
+	CMyMatrix j_matrix = jacobi(x, f_1);
+	j_matrix.print_matrix(j_matrix);
+*/
+	//-------------------------------------------------------------------------------------------
+	//Aufgabe 3
+
+	//CMyVektor x(2); //startwert
+	//x.set_component(0, 1);
+	//x.set_component(1, 1); 
+
+	//newton_verfahren(x, f_2);
+
+	//-------------------------------------------------------------------------------------------
+	
 	return 0;
 	system("PAUSE");
 }
