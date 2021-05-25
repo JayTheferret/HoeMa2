dcf330ce6e793bd5869906c283eb39283a8ec139
Jay <57915168+JayTheferret@users.noreply.github.com>
2021-05-25 11:34:37 +0200

    finished differences

--- Praktikum 3/main.cpp
+++ Praktikum 3/main.cpp
@@ -20,34 +20,35 @@ int main() {
 
 	//Beispiel Heun-Verfahren
 
-	CMyVektor startw_1(2);
+	/*CMyVektor startw_1(2);
 
 	startw_1.set_component(0, 0);
 	startw_1.set_component(1, 1);
 
 	C_DGLSolver solver1(f_DGL);
 
-	solver1.heun(startw_1, 0, 2, 100);
+	solver1.heun(startw_1, 0, 2, 100);*/
 
 	//----------------------------------------------
 	//----------------------------------------------
 
 	//Abweichung
-	/*CMyVektor y_start(2);
+	CMyVektor y_start(3);
 
 	y_start.set_component(0, 1);
 	y_start.set_component(1, -1);
 	y_start.set_component(2, 2);
 
-	double exact_result = 0.5;
-	C_DGLSolver solver = C_DGLSolver(f_DGL_3terO);
+	//double exact_result = 0.5;
+	C_DGLSolver solver(f_DGL_3terO);
+	//solver.euler_oa(y_start, 0, 2, 10).print_vector(solver.euler_oa(y_start, 1, 2, 10));
 
-	for (int i = 10; i < 1000; i *= 10) {
-		std::cout << "Abweichung bei Euler bei " << i << " Schritten: " << solver.euler(y_start, 1, 2, i)[0] - exact_result << std::endl
-			<< "Abweichung bei Heun bei " << i << " Schritten: " << std::endl;
+	for (int i = 10; i < 100000; i *= 10) {
+		std::cout	<< "Abweichung bei Euler bei " << i << " Schritten: " << solver.euler_oa(y_start, 1, 2, i)[0] - 0.5 << std::endl
+					<< "Abweichung bei Heun bei "  << i << " Schritten: " << solver.heun_oa(y_start, 1, 2, i)[0] - 0.5 << std::endl;
 
+		//y(2) = 0.5 (1/x)
 	}
-*/
 
 	system("PAUSE");
 	return 0;
